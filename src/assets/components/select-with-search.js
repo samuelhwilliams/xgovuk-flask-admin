7facb15489c4b23dfd9bafcd0f9b270ca1109436
Samuel Williams <samuelhwilliams@gmail.com>
2025-10-28 23:09:40 +0000

    Sync select-with-search accessibility update

--- src/assets/components/select-with-search.js
+++ src/assets/components/select-with-search.js
@@ -2,7 +2,7 @@
  * Select with Search Component
  *
  * Based on govuk_publishing_components select-with-search component
- * Source: https://github.com/alphagov/govuk_publishing_components/blob/main/app/assets/javascripts/govuk_publishing_components/components/select-with-search.js
+ * Source: https://github.com/alphagov/govuk_publishing_components/blob/6e58485f5219508e4f0fc7db465bdadb58a45d29/app/assets/javascripts/govuk_publishing_components/components/select-with-search.js
  *
  * Uses Choices.js for the enhanced select functionality
  * Choices.js: https://github.com/Choices-js/Choices
@@ -38,27 +38,34 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
                 : "Select one";
         }
 
-        const ariaDescribedBy =
-            this.module.getAttribute("aria-describedby") || "";
+        const ariaDescribedBy = this.module.getAttribute("aria-describedby") || "";
+        const labelId = this.module.id + "-label " + ariaDescribedBy;
 
         this.choices = new Choices(this.module, {
             allowHTML: false,
             searchPlaceholderValue: "Search in list",
-            shouldSort: false,
+            shouldSort: false, // show options and groups in the order they were given
             itemSelectText: "",
             searchResultLimit: 100,
             removeItemButton: this.module.multiple,
-            labelId: this.module.id + "-label " + ariaDescribedBy,
+            labelId: labelId,
             callbackOnInit: function () {
+                // For the multiple select, move the input field to
+                // the top of the feedback area, so that the selected
+                // 'lozenges' appear afterwards in a more natural flow
                 if (this.dropdown.type === "select-multiple") {
                     const inner = this.containerInner.element;
                     const input = this.input.element;
                     inner.prepend(input);
                 }
+                // Add aria-labelledby to the listbox as well as the combobox
+                const listbox = this.itemList.element;
+                listbox.setAttribute("aria-labelledby", labelId);
             },
+            // https://fusejs.io/api/options.html
             fuseOptions: {
-                ignoreLocation: true,
-                threshold: 0,
+                ignoreLocation: true, // matches any part of the string
+                threshold: 0, // only matches when characters are sequential
             },
         });
 
